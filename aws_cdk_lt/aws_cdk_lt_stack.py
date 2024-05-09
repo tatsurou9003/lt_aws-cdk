50fdc600c6b4e2a85deeb83cd185d5273bf4ed31
tatsurou9003 <t.murakami@4nonome.com>
2024-05-09 16:07:48 +0900

    lambda, apigateway

--- aws_cdk_lt/aws_cdk_lt_stack.py
+++ aws_cdk_lt/aws_cdk_lt_stack.py
@@ -1,19 +1,28 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    Duration,
+    aws_lambda as lambda_,
+    aws_lambda_event_sources as lambda_event_sources,
 )
 from constructs import Construct
+
+function_timeout = 3
+function_memory_size = 128
 
 class AwsCdkLtStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsCdkLtQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        lambda_function = lambda_.Function(self, "HelloWorldFunction",
+            code=lambda_.Code.from_asset("lib"),
+            handler="lambda_handler.lambda_handler",
+            runtime=lambda_.Runtime.PYTHON_3_8,
+            timeout=Duration.minutes(function_timeout),
+            memory_size=function_memory_size,
+        )
+        lambda_function.add_event_source(lambda_event_sources.ApiEventSource(
+                method="get",
+                path="/hello",
+            )
+        )
